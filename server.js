2b75967f60d55b61b90d9c0110e5f55c8d8d75c1
Kyle Jeffrey <kajeffre@ucsc.edu>
2020-07-15 13:36:54 -0700

    only static pages allowed

--- server.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const app = express();
-
-const PORT = process.env.PORT || 30000;
-app.use(express.static('public'));
-
-app.listen(PORT, () => {
-    console.log('Now listening on port ' + PORT);
-});
