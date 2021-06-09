79c737cc50ab2e459c6f7633a401f671fe31c89a
AlphaBeard <theawesomeguynextdoor@gmail.com>
2021-06-09 15:42:59 -0700

    updated

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -264,7 +264,7 @@
             percentPosition: true,
             masonry: {
                 // use outer width of grid-sizer for columnWidth
-                columnWidth: '.prt-grid',
+                // columnWidth: '.prt-grid',
             }
         });
     });
